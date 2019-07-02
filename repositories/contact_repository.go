7542bc8ec2261cc12175872b40a132e257e168b8
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2019-07-02 19:01:29 +0700

    create contact repository

---
+++ repositories/contact_repository.go
@@ -0,0 +1,24 @@
+package repositories
+
+import (
+	"github.com/herusdianto/gorm_crud_example/models"
+	"github.com/jinzhu/gorm"
+)
+
+type ContactRepository struct {
+	db *gorm.DB
+}
+
+func NewContactRepository(db *gorm.DB) *ContactRepository {
+	return &ContactRepository{db: db}
+}
+
+func (r *ContactRepository) Save(contact *models.Contact) RepositoryResult {
+	err := r.db.Save(contact).Error
+
+	if err != nil {
+		return RepositoryResult{Error: err}
+	}
+
+	return RepositoryResult{Result: contact}
+}
