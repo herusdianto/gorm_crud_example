c15d6687caf7827f8c5ccefa8994d1f9c5b84b4c
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2019-07-03 13:14:55 +0700

    add validation to contact model

--- models/contact.go
+++ models/contact.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Contact struct {
-	ID        string `gorm:"primary_key"`
-	Name      string `gorm:"type:varchar(255);NOT NULL"`
-	Email     string `gorm:"type:varchar(255)"`
-	Phone     string `gorm:"type:varchar(100);NOT NULL;UNIQUE;UNIQUE_INDEX"`
-	Address   string `gorm:"type:text"`
+	ID        string `gorm:"primary_key" json:"id"`
+	Name      string `gorm:"type:varchar(255);NOT NULL" json:"name" binding:"required"`
+	Email     string `gorm:"type:varchar(255)" json:"email"`
+	Phone     string `gorm:"type:varchar(100);NOT NULL;UNIQUE;UNIQUE_INDEX" json:"phone" binding:"required"`
+	Address   string `gorm:"type:text" json:"address"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
